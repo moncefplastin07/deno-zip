aeb95e035044088612f30662da9e317c950715f6
Moncef Gaha <moncefplastin07@gmail.com>
2020-10-06 18:16:46 +0100

    fix a bug

--- mod.ts
+++ mod.ts
@@ -1,2 +1,1 @@
 export { unZipFromFile } from "./unzip.ts";
-
--- unzip.ts
+++ unzip.ts
@@ -1,29 +1,50 @@
-import { join, exists } from "./deps.ts";
-export const unZipFromFile = async (filePath: string, destinationPath: string = './', options: any = {}): Promise<string | false>=> {
+import { exists, join } from "./deps.ts";
+export const unZipFromFile = async (
+  filePath: string,
+  destinationPath: string | null = "./",
+  options: any = {},
+): Promise<string | false> => {
+  // check if the zip file is exist
+  if (!await exists(filePath)) {
+    return false;
+  }
+  // check destinationPath is not null and set './' as destinationPath
+  if (!destinationPath) {
+    destinationPath = "./";
+  }
+  //get the file name from filePath
+  const fullFileName = filePath.split("/");
+  // the file name with aut extension
+  const fileNameWithOutExt =
+    fullFileName[fullFileName.length - 1].split(".")[0];
+  // get the extract file and add fileNameWithOutExt whene options.includeFileName is true
+  const fullDestinationPath = options.includeFileName
+    ? join(destinationPath, fileNameWithOutExt)
+    : destinationPath;
 
-    // check if the zip file is exist
-    if (!await exists(filePath)) {
-        return false
-    }
-    //get the file name from filePath
-    const fullFileName = filePath.split('/')
-    // the file name with aut extension
-    const FileNameWithOutExt = fullFileName[fullFileName.length - 1].split('.')[0]
-    // get the extract file and add FileNameWithOutExt whene options.includeFileName is true
-    const fullDestinationPath = options.includeFileName ? join(destinationPath, FileNameWithOutExt) : destinationPath
+  // return the unzipped file path or false whene the unzipping Process failed
+  return await unzipProcess(filePath, fullDestinationPath)
+    ? fullDestinationPath
+    : false;
+};
+const unzipProcess = async (
+  zipSourcePath: string,
+  destinationPath: string,
+): Promise<boolean> => {
+  const unzipCommandProcess = Deno.run({
+    cmd: Deno.build.os === "windows"
+      ? [
+        "PowerShell",
+        "Expand-Archive",
+        "-Path",
+        zipSourcePath,
+        "-DestinationPath",
+        destinationPath,
+      ]
+      : ["unzip", zipSourcePath, "-d", destinationPath],
+    stdout: "piped",
+    stderr: "piped",
+  });
 
-    // return the unzipped file path or false whene the unzipping Process failed
-    return await unzipProcess(filePath, destinationPath) ? fullDestinationPath : false
-
-}
-const unzipProcess = async (zipSourcePath: string, destinationPath: string ): Promise<boolean> =>{
-    const unzipCommandProcess = Deno.run({
-        cmd: Deno.build.os === 'windows' ? ["PowerShell", "Expand-Archive", "-Path", zipSourcePath, "-DestinationPath", destinationPath] : ["unzip", zipSourcePath, "-d", destinationPath],
-        stdout: "piped",
-        stderr: "piped"
-    })
-    
-    return (await unzipCommandProcess.status()).success
-    
-}
-
+  return (await unzipCommandProcess.status()).success;
+};
