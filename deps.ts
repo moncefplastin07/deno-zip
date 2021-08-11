2dc4f9bf40c4960f968be7c9d4e6d42d3391de4f
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-11 11:25:47 +0100

    upgrade to std 0.104.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { join } from "https://deno.land/std@0.85.0/path/mod.ts";
-export { exists } from "https://deno.land/std@0.85.0/fs/mod.ts";
-export * as log from "https://deno.land/std@0.85.0/log/mod.ts";
+export { join } from "https://deno.land/std@0.104.0/path/mod.ts";
+export { exists } from "https://deno.land/std@0.104.0/fs/mod.ts";
+export * as log from "https://deno.land/std@0.104.0/log/mod.ts";
