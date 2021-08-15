1e069e82f9a24609e480d10c2a1114f76597ae34
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-15 14:44:17 +0100

    remove loge dependency

--- deps.ts
+++ deps.ts
@@ -1,3 +1,2 @@
 export { join } from "https://deno.land/std@0.104.0/path/mod.ts";
 export { exists } from "https://deno.land/std@0.104.0/fs/mod.ts";
-export * as log from "https://deno.land/std@0.104.0/log/mod.ts";
