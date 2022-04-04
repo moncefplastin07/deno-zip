0a8a1edaa8c12f464b2b980ab7b96911340ad232
moncefplastin07 <moncefplastin07@gmail.com>
2022-04-04 11:28:58 +0100

    Bump to deno std@0.133.0

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { join } from "https://deno.land/std@0.104.0/path/mod.ts";
-export { exists } from "https://deno.land/std@0.104.0/fs/mod.ts";
+export { join } from "https://deno.land/std@0.133.0/path/mod.ts";
+export { exists } from "https://deno.land/std@0.133.0/fs/mod.ts";
